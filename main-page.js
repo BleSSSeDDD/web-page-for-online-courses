96b4386ae886147b0082d7b9556581394905714c
BleSSSeDDD <120226159+BleSSSeDDD@users.noreply.github.com>
2024-08-20 11:55:23 +0300

    Add files via upload

---
+++ main-page.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("cookie-banner").style.display = "block";
+    }
+    document.getElementById("cookie-accept").addEventListener("click", function() {
+        document.getElementById("cookie-banner").style.display = "none"; 
+    });
