31a27092bf08df570f0604ae86d245501c190ee9
BleSSSeDDD <120226159+BleSSSeDDD@users.noreply.github.com>
2024-08-28 14:38:52 +0300

    Delete main-page.js

--- main-page.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-            document.getElementById("cookie-banner").style.display = "block";
-        }
-
-        // Обработчик для принятия файлов cookie
-        document.getElementById("cookie-accept").addEventListener("click", function() {
-            document.getElementById("cookie-banner").style.display = "none"; // Скрываем баннер
-        });
