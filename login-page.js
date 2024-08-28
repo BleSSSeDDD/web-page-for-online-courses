a3e2ec2a151a1ffcab056b76814ed68fd2c8b5ca
BleSSSeDDD <120226159+BleSSSeDDD@users.noreply.github.com>
2024-08-28 14:38:32 +0300

    Delete login-page.js

--- login-page.js
+++
@@ -1,9 +0,0 @@
-function showPassword() {
-    const passwordInput = document.getElementById('password');
-    passwordInput.type = 'text';
-}
-
-function hidePassword() {
-    const passwordInput = document.getElementById('password');
-    passwordInput.type = 'password';
-}
